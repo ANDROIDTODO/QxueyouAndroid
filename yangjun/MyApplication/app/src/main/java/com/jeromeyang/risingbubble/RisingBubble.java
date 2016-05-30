4d839b7907b87060f4a73806d6a4369ef84ee043
jeromeyang <jeeromeyang@gmail.com>
2016-05-30 20:28:11 +0800

    init project

---
+++ yangjun/MyApplication/app/src/main/java/com/jeromeyang/risingbubble/RisingBubble.java
@@ -0,0 +1,23 @@
+package com.jeromeyang.risingbubble;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+import android.view.View;
+
+/**
+ * Created by Jeromeyang on 16/5/30.
+ */
+public class RisingBubble extends SurfaceView {
+    public RisingBubble(Context context) {
+        super(context);
+    }
+
+    public RisingBubble(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RisingBubble(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
